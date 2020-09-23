3cd70d3277eac628d95ab3805e5ceb3ca584a22d
vertefra <verte.fra@gmail.com>
2020-09-23 08:58:16 -0400

    connection established

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,55 @@
 import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
 const ENDPOINT = "http://127.0.0.1:5000/";
+// const socket = io.connect(ENDPOINT, { reconnection: true });
 
 export default function Main() {
-  const [response, setResponse] = useState("");
-  const socket = io.connect(ENDPOINT, { reconnection: true });
+  const [user, setUser] = useState({
+    username: "",
+    sid: "",
+  });
+
+  const handleChange = (e) => {
+    setUser({
+      [e.target.id]: e.target.value,
+      ...user.sid,
+    });
+  };
+
+  const connect = (e) => {
+    e.preventDefault();
+    const private_socket = io.connect(ENDPOINT, { reconnection: true });
+    private_socket.emit("connect");
+    private_socket.on("connected", (data) => {
+      setUser({
+        ...user,
+        sid: data.sid,
+      });
+      private_socket.emit("join", { user });
+    });
+    // private_socket.emit("join", { user });
+    // private_socket.on("connection_established", (data) => {
+    //   console.log(data);
+    // });
+  };
+
+  // established first connection and get session id
 
   useEffect(() => {
-    socket.on("connect", (message) => {
-      console.log(message);
-      setResponse(message);
-    });
-  }, []);
-
+    console.log(user);
+  }, [user]);
   return (
     <div>
-      <button>click</button>
-      <p> {response}</p>
+      <form>
+        <input
+          type="text"
+          id="username"
+          value={user.username}
+          onChange={handleChange}
+          placeholder="username"
+        />
+        <input type="submit" value="connect" onClick={connect} />
+      </form>
     </div>
   );
 }
