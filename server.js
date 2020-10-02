421a8cc608b8ee853b957ca628317242612d6f50
vertefra <verte.fra@gmail.com>
2020-10-02 12:22:41 -0400

    server added

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const SSL_PORT = 443;
+const HTTP_PORT = 80;
+
+app.use(express.static("build"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(`${__dirname}/build/index.html`));
+});
+
+app.listen(SSL_PORT && HTTP_PORT);
